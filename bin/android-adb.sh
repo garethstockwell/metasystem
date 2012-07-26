53c74507592faeca7b75e035b215dab8c1063d03
Gareth Stockwell <gareth.stockwell.dev@gmail.com>
2012-07-26 09:15:18 +0100

    android-adb.sh: use mintty for 'adb logcat' on Windows

--- bin/android-adb.sh
+++ bin/android-adb.sh
@@ -2,11 +2,21 @@
 
 # Wrapper for adb which redirects logcat to a colorizer
 
-logcat=
-[[ $1 == logcat ]] && shift && logcat=1
-if [[ -n $logcat ]]; then
-	android-adb-logcat.py "$@"
-else
-	adb "$@"
-fi
+case $1 in
+	logcat)
+		shift
+		android-adb-logcat.py "$@"
+		;;
+	shell)
+		# Console2 doesn't handle terminal control codes well
+		if [[ $METASYSTEM_OS == windows ]]; then
+			nohup mintty -e adb "$@" &
+		else
+			adb "$@"
+		fi
+		;;
+	*)
+		adb "$@"
+		;;
+esac
 
