e0ef4be6de77bddfe8f1d2d4ef627bf53d3d88f2
Gareth Stockwell <gareth.stockwell.dev@gmail.com>
2012-08-01 14:05:57 +0100

    android-adb.sh: fixed mintty launching

--- bin/android-adb.sh
+++ bin/android-adb.sh
@@ -8,9 +8,23 @@ case $1 in
 		android-adb-logcat.py "$@"
 		;;
 	shell)
-		# Console2 doesn't handle terminal control codes well
 		if [[ $METASYSTEM_OS == windows ]]; then
-			nohup mintty -e adb "$@" &
+			if [[ -z $2 ]]; then
+				# Console2 doesn't handle terminal control codes well
+				mintty_dir=
+				case $METASYSTEM_PLATFORM in
+					mingw)
+						mintty_dir=/c/cygwin/bin
+						;;
+					cygwin)
+						mintty_dir=/usr/bin
+						;;
+				esac
+				nohup $mintty_dir/mintty.exe -e adb "$@" &
+			else
+				# Remove trailing CR
+				adb "$@" | tr -d '\015'
+			fi
 		else
 			adb "$@"
 		fi
