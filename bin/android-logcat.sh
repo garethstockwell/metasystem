571fb13d02ff5c312d1e4bb979399796d1479e9a
Gareth Stockwell <gareth.stockwell.dev@gmail.com>
2012-12-07 12:26:38 +0000

    Added android-logcat.sh

---
+++ bin/android-logcat.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Capture Android log into a uniquely named file
+
+#------------------------------------------------------------------------------
+# Constants
+#------------------------------------------------------------------------------
+
+LOG_FILENAME_FORMAT='alog_%02d.txt'
+
+
+#------------------------------------------------------------------------------
+# Functions
+#------------------------------------------------------------------------------
+
+function get_filename()
+{
+	local i=1
+	while (true); do
+		local file=$(printf $LOG_FILENAME_FORMAT $i)
+		i=$((i+1))
+		if [[ ! -e $file ]]; then
+			echo $file
+			break
+		fi
+	done
+}
+
+# Print an error message and exit
+function error()
+{
+    echo -e "\nError: $*"
+    if [[ "$opt_dryrun" != yes ]]; then
+        exit 1
+    fi
+}
+
+
+#------------------------------------------------------------------------------
+# Main
+#------------------------------------------------------------------------------
+
+file=$(get_filename)
+echo -e "Log filename $file"
+
+echo -en "Waiting for device ... "
+adb wait-for-device
+echo -e "OK"
+
+devices=$(metasystem_android_devices)
+[[ -z $devices ]] && error "No ADB device found"
+[[ $(echo $devices | wc -w) != 1 ]] && error "Multiple ADB devices found: $devices"
+
+echo -e "Clearing device log ..."
+adb shell logcat -c
+
+echo -e "Starting logcat ..."
+adb logcat -v threadtime | tee $file
+
