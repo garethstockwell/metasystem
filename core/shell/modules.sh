e89a03774a8b8806b25306ac4a074ef6c7b0ea80
Gareth Stockwell <gareth.stockwell.dev@gmail.com>
2012-12-20 10:45:50 +0000

    Moved all modules into directories

--- core/shell/modules.sh
+++ core/shell/modules.sh
@@ -9,33 +9,25 @@ function metasystem_module_load()
 	local module=$1
 	local quiet=
 	[[ $2 == '-quiet' ]] && quiet=1
-	local file=$METASYSTEM_ROOT/modules/$module.sh
-	local dir=$METASYSTEM_ROOT/modules/$module
-	[[ ! -f $METASYSTEM_ROOT/modules/$module.sh ]] && unset file
-	[[ ! -d $METASYSTEM_ROOT/modules/$module ]] && unset dir
-	if [[ -n $file && -n $dir ]]; then
-		echo "Error: $module is ambiguous" >&2
-		return 1
-	fi
-	if [[ -z $file && -z $dir ]]; then
+	local script=$METASYSTEM_ROOT/modules/$module/module.sh
+	if [[ -e $script ]]; then
+		if [[ -z $quiet ]]; then
+			echo "Loading module $module ..."
+		fi
+		source $script
+	else
 		echo "Error: $module not found" >&2
 		return 1
 	fi
-	local script=$file
-	[[ -n $dir ]] && script=$dir/module.sh
-	if [[ -z $quiet ]]; then
-		echo "Loading module $module ..."
-	fi
-	source $script
 }
 
 function metasystem_module_load_all()
 {
 	local first=1
-	for entry in $('ls' $METASYSTEM_ROOT/modules | grep -v ^templates); do
+	for script in $(find $METASYSTEM_ROOT/modules -iname module.sh | sed -e "s|$METASYSTEM_ROOT/modules/||" | grep -v ^template); do
 		[[ -n $first ]] && _metasystem_print_banner Modules
 		unset first
-		local module=${entry/.sh/}
+		local module=$(dirname $script)
 		echo -n $module
 		if [[ -z $(list_contains $module $METASYSTEM_MODULES_DISABLE) ]]; then
 			echo
