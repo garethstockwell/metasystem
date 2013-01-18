dea5dead7c9f5ebbf78bb3d974361183f3e2edb1
Gareth Stockwell <gareth.stockwell.dev@gmail.com>
2013-01-18 15:16:43 +0000

    ssh: fixed ssh-wrapper.sh for Windows

--- modules/ssh/bin/ssh-wrapper.sh
+++ modules/ssh/bin/ssh-wrapper.sh
@@ -3,5 +3,7 @@
 SSH_AGENT_PID=
 SSH_AGENT_ENV="$HOME/.ssh/agent-env-${HOSTNAME}-${METASYSTEM_OS}-${METASYSTEM_PLATFORM}"
 [[ -f $SSH_AGENT_ENV ]] && source $SSH_AGENT_ENV
-$(which ssh) "$@"
+ssh_exe=/bin/ssh
+[[ $OS != Windows_NT ]] && ssh_exe=$(which ssh)
+$ssh_exe "$@"
 
