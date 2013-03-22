5b3adf85f8b8577109bc21b7d1a2fe74be206586
Gareth Stockwell <gareth.stockwell.dev@gmail.com>
2013-03-22 14:28:26 +0000

    core/test/console.py: fixed

--- core/test/console.py
+++ core/test/console.py
@@ -9,35 +9,35 @@ sys.path.append(os.path.join(sys.path[0], '../lib/python'))
 import Console
 
 def print_break():
-    Console.stdout.write("\n")
-    Console.stderr.write("\n")
+    sys.stdout.write("\n")
+    sys.stderr.write("\n")
 
-Console.stdout.write("out-default-default\n")
-Console.stderr.write("err-default-default\n")
-
-print_break()
-
-Console.stdout.set_fg(Console.RED)
-Console.stdout.write("out-red-default\n")
-Console.stderr.write("err-default-default\n")
+sys.stdout.write("out-default-default\n")
+sys.stderr.write("err-default-default\n")
 
 print_break()
 
-Console.stdout.set_bg(Console.WHITE)
-Console.stdout.write("out-red-white\n")
-Console.stderr.write("err-default-default\n")
+sys.stdout.state.set_fg(Console.Color.RED)
+sys.stdout.write("out-red-default\n")
+sys.stderr.write("err-default-default\n")
 
 print_break()
 
-Console.stderr.set_fg(Console.BLUE)
-Console.stdout.write("out-red-white\n")
-Console.stderr.write("err-blue-default\n")
+sys.stdout.state.set_bg(Console.Color.WHITE)
+sys.stdout.write("out-red-white\n")
+sys.stderr.write("err-default-default\n")
 
 print_break()
 
-Console.stderr.set_bg(Console.YELLOW)
-Console.stdout.write("out-red-white\n")
-Console.stderr.write("err-blue-yellow\n")
+sys.stderr.state.set_fg(Console.Color.BLUE)
+sys.stdout.write("out-red-white\n")
+sys.stderr.write("err-blue-default\n")
+
+print_break()
+
+sys.stderr.state.set_bg(Console.Color.YELLOW)
+sys.stdout.write("out-red-white\n")
+sys.stderr.write("err-blue-yellow\n")
 
 print_break()
 
