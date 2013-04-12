d2f6c483a34d803f222adb66eee6ec519d1fa405
Gareth Stockwell <gareth.stockwell.dev@gmail.com>
2013-04-12 12:39:26 +0100

    cygwin: trying to improve mount setup

--- core/shell/shrc-cygwin.sh
+++ core/shell/shrc-cygwin.sh
@@ -11,9 +11,16 @@ export DISPLAY=127.0.0.1:0.0
 # Custom mounts
 #------------------------------------------------------------------------------
 
-if [[ -z `mount | grep desktop` ]]; then
-	desktop=`cygpath -D -w`
-	echo "Desktop = $desktop"
-	mount "$desktop" /desktop
-fi
+function metasystem_cygwin_mount()
+{
+	local path=$1
+	local mountpoint=$2
+	if [[ -z `mount | grep $mountpoint` ]]; then
+		echo "Mounting $path at $mountpoint"
+		mount -f "$(metasystem_nativepath $path)" "$mountpoint"
+	fi
+}
+
+metasystem_cygwin_mount $(cygpath -D -w) /desktop
+metasystem_cygwin_mount ${HOME}/work/sync/git ${HOME}/git
 
