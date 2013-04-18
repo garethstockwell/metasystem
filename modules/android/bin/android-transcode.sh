087dc2b981b2c8f7dc778816dfa2e61cf0729d8f
Gareth Stockwell <gareth.stockwell.dev@gmail.com>
2013-04-18 08:20:24 -0700

    android-transcode.sh: allow ffmpeg/avconv; switch to h.264

--- modules/android/bin/android-transcode.sh
+++ modules/android/bin/android-transcode.sh
@@ -2,12 +2,15 @@
 
 # Transcode a short clip for testing on Android emulator
 
+exe=avconv
+[[ -z $(which $exe) ]] && exe=ffmpeg
+
 in_file=$1
 out_file=$2
 
-avconv \
+$exe \
 	-i $in_file \
-	-vcodec mpeg4 -b 1000k \
+	-vcodec libx264 -b 1000k \
 	-acodec aac -ar 32000 -ab 128k -ac 2 \
 	-strict experimental \
 	-t 00:01:00 \
