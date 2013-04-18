ce0cd3e83b48fdbd2a0a49bbd611801e9e714997
Gareth Stockwell <gareth.stockwell.dev@gmail.com>
2013-04-18 05:56:05 -0700

    android-transcode.sh: changed audio codec to AAC

--- modules/android/bin/android-transcode.sh
+++ modules/android/bin/android-transcode.sh
@@ -8,7 +8,8 @@ out_file=$2
 avconv \
 	-i $in_file \
 	-vcodec mpeg4 -b 1000k \
-	-acodec ac3 -ar 32000 -ab 128k -ac 2 \
+	-acodec aac -ar 32000 -ab 128k -ac 2 \
+	-strict experimental \
 	-t 00:01:00 \
 	$out_file
 
