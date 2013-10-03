554a66bf23381ef699b409ff0d2dc3e15440acef
Gareth Stockwell <gareth.stockwell.dev@gmail.com>
2013-10-03 14:33:31 +0100

    shrc: support completion for aliases

--- core/shell/bashrc.sh
+++ core/shell/bashrc.sh
@@ -4,11 +4,35 @@
 # Used as e.g. "local ${OVERRIDE_SPECIAL} path=something"
 OVERRIDE_SPECIAL=
 
+#------------------------------------------------------------------------------
+# Completion
+#------------------------------------------------------------------------------
+
+# http://unix.stackexchange.com/questions/4219/how-do-i-get-bash-completion-for-command-aliases
+function declare_bash_completion_wrapper()
+{
+	local function_name="$2"
+	local arg_count=$(($#-3))
+	local comp_function_name="$1"
+	shift 2
+	local function="
+	function $function_name()
+	{
+		((COMP_CWORD+=$arg_count))
+		COMP_WORDS=( "$@" \${COMP_WORDS[@]:1} )
+		"$comp_function_name"
+		return 0
+	}"
+	eval "$function"
+}
+
+
+#------------------------------------------------------------------------------
+# Cross-shell rc
+#------------------------------------------------------------------------------
+
 source $(dirname ${BASH_SOURCE[0]})/shrc.sh
 
-#==============================================================================
-# Core setup 1/2
-#==============================================================================
 
 #------------------------------------------------------------------------------
 # Bash feature flags
