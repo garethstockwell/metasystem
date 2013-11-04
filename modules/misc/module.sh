23e67b8423a89cd158a6f7a396df43007695f62c
Gareth Stockwell <gareth.stockwell.dev@gmail.com>
2013-11-04 09:18:41 +0000

    Add functions for removing Firefox, Skype lock files

--- modules/misc/module.sh
+++ modules/misc/module.sh
@@ -4,6 +4,24 @@
 # Functions
 #------------------------------------------------------------------------------
 
+# Run to fix erroneous "you are already logged in on this computer" login
+# failure
+function skype_remove_lock()
+{
+	local skype_dir=$HOME/.Skype
+	rm -fv $(find ${skype_dir} -iname *.lock)
+	rm -fv $(find ${skype_dir} -iname *.lck)
+}
+
+# Run to fix erroneous "Firefox already running" startup failure
+function firefox_remove_lock()
+{
+	local firefox_dir=$HOME/.mozilla/firefox
+	rm -fv $(find ${firefox_dir} -iname lock)
+	rm -fv $(find ${firefox_dir} -iname .parentlock)
+}
+
+
 
 #------------------------------------------------------------------------------
 # Exported variables
