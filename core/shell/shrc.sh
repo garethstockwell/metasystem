6c539f296086caf96cdf4751911481e237db966f
Gareth Stockwell <gareth.stockwell.dev@gmail.com>
2013-12-20 06:37:39 +0000

    shrc: fix METASYSTEM_XXX variable initialization

--- core/shell/shrc.sh
+++ core/shell/shrc.sh
@@ -38,33 +38,6 @@ _metasystem_print_rule
 _metasystem_start_dir=$PWD
 builtin cd $HOME
 
-export METASYSTEM_CORE_ROOT=$METASYSTEM_ROOT/core
-export METASYSTEM_CORE_BIN=$METASYSTEM_CORE_ROOT/bin
-export METASYSTEM_CORE_LIB=$METASYSTEM_CORE_ROOT/lib
-export METASYSTEM_CORE_SHELL=$METASYSTEM_CORE_ROOT/shell
-export METASYSTEM_CORE_TEMPLATES=$METASYSTEM_CORE_ROOT/templates
-
-export METASYSTEM_CORE_LIB_BASH=$METASYSTEM_CORE_LIB/bash
-
-if [[ -n $METASYSTEM_LOCAL_ROOT ]]; then
-	metasystem_local_root=$METASYSTEM_LOCAL_ROOT
-else
-	metasystem_local_root=$METASYSTEM_ROOT/../metasystem-local
-fi
-
-if [[ -d $metasystem_local_root ]]; then
-	export METASYSTEM_LOCAL_ROOT=$(cd $metasystem_local_root && pwd)
-	export METASYSTEM_LOCAL_BIN=$METASYSTEM_LOCAL_ROOT/bin
-	export METASYSTEM_LOCAL_LIB=$METASYSTEM_LOCAL_ROOT/lib
-	export METASYSTEM_LOCAL_SHELL=$METASYSTEM_LOCAL_ROOT/shell
-	export METASYSTEM_LOCAL_TEMPLATES=$METASYSTEM_LOCAL_ROOT/templates
-	export METASYSTEM_CORE_CONFIG=$METASYSTEM_LOCAL_ROOT/config
-
-	export METASYSTEM_LOCAL_LIB_BASH=$METASYSTEM_LOCAL_LIB/bash
-else
-	export METASYSTEM_CORE_CONFIG=$METASYSTEM_CORE_TEMPLATES/local/config
-fi
-
 
 #------------------------------------------------------------------------------
 # Local profile
