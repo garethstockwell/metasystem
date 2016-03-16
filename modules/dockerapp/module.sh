7d4af369c5e57b0c37173f0b72a1b0852cb3ea71
Gareth Stockwell <gareth.stockwell.dev@gmail.com>
2016-03-16 08:57:38 +0000

    dockerapp: remove console spam

--- modules/dockerapp/module.sh
+++ modules/dockerapp/module.sh
@@ -4,8 +4,6 @@ if [ -z $DOCKERAPP_SETUP_SCRIPT ]; then
     DOCKERAPP_SETUP_SCRIPT=$METASYSTEM_ROOT/../dockerapp/shell/shrc.sh
 fi
 
-echo $DOCKERAPP_SETUP_SCRIPT
-
 if [ -e $DOCKERAPP_SETUP_SCRIPT ]; then
 	source $DOCKERAPP_SETUP_SCRIPT
 fi
