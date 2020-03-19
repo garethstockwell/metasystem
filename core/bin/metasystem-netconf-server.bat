7b5e3f02bd42949e8a98532ae94c32add425ec5c
Gareth Stockwell <gareth.stockwell.dev@gmail.com>
2020-03-19 16:22:05 +0000

    core/bin: delete unused scripts

--- core/bin/metasystem-netconf-server.bat
+++
@@ -1,7 +0,0 @@
-REM For service installation, see
-REM http://agiletesting.blogspot.co.uk/2005/09/running-python-script-as-windows.html
-
-set METASYSTEM_HOME=c:\Users\garsto01\work\sync\git\github\metasystem
-
-c:\apps\python\2.7\python.exe %METASYSTEM_HOME%\core\bin\metasystem-netconf-server.py start --fg
-
